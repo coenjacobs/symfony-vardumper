75a19f7f15f5813021ef89ce73612113ac9c96d2
Coen Jacobs <coenjacobs@gmail.com>
2015-03-06 10:50:53 +0100

    Initial commit

---
+++ wordpress-symfony-vardumper.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Plugin Name: WordPress Symfony VarDumper
+ * Description: Brings the Symfony VarDumper component to your WordPress installation.
+ * Author: Coen Jacobs
+ * Author URI: http://coenjacobs.me
+ * Version: 1.0.0
+ */
+
+include( 'vendor/autoload.php' );
+
+$updatePhp = new WPUpdatePhp( '5.4.0' );
+
+if ( $updatePhp->does_it_meet_required_php_version( phpversion() ) ) {
+	if ( function_exists('dd') ) {
+		return;
+	}
+
+	function dd( $data ) {
+		dump( $data );
+		die();
+	}
+}
